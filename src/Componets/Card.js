a88180bd2702f5dfeae6b9022cc7f55bc4e54980
Elegido A dedo <elegidoadedo@MacBook-Pro-E.local>
2019-10-13 21:13:33 +0200

    Added default valur for cards and key in map method

--- src/Componets/Card.js
+++ src/Componets/Card.js
@@ -1,14 +1,12 @@
 import React from 'react';
 
-const Cards = ({ cards }) => {
-    console.log({cards})
-
+const Cards = ({ cards = [] }) => {
     return (
         <div class="row">
        {cards.map((card) => (
-            <span>
-                { card.data.crosspost_parent == null && card.data.media == null ? 
-                <div class="card mb-4">
+            <span key={card.data.id}>
+                { card.data.crosspost_parent == null && card.data.media == null 
+                ? <div class="card mb-4">
                     <img src={ card.data.url } alt="" width="400px" height="300px"/>
                 </div>
                 : ""
