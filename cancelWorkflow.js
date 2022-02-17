78e61f5ede6eddb6d22727219c5ef04630a3ce3c
johanna <johanna@circleci.com>
2022-02-16 16:07:29 -0800

    fix second buildArgs

--- cancelWorkflow.js
+++ cancelWorkflow.js
@@ -6,7 +6,7 @@ console.log(buildArgs)
 const options = {
   method: 'POST',
   url: 'https://circleci.com/api/v2/project/gh/jkzilla/circleci-demo-javascript-react-app/' + buildArgs[0] + '/cancel',
-  headers: {'Circle-Token': process.env.buildArgs[1]}
+  headers: {'Circle-Token': buildArgs[1]}
 };
 
 request(options, function (error, response, body) {
